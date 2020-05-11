0fb584a6c7420236be0919bfe166330ce466f7c3
volantgoat <507085831@qq.com>
2020-05-11 16:12:57 +0800

    MainActivity

---
+++ app/src/main/java/com/volantgoat/onesentence/Activity/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.volantgoat.onesentence.Activity.Main;
+
+import com.volantgoat.onesentence.Base.BaseActivity;
+import com.volantgoat.onesentence.R;
+
+/**
+ * Create by dong
+ * Date on 2020/5/11  16:11
+ */
+public class MainActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+}
